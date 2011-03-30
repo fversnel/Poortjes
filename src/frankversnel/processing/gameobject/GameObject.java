37b952b7a4683f8fc83c039340314f8e4c7e0cee
Frank Versnel <f.versnel@gmail.com>
2011-03-30 23:12:09 +0200

    GameObject.getComponent now returns null if no components are available instead of throwing an IndexOutOfBoundsException.

--- src/frankversnel/processing/gameobject/GameObject.java
+++ src/frankversnel/processing/gameobject/GameObject.java
@@ -34,14 +34,20 @@ public class GameObject {
 
 	@SuppressWarnings("unchecked")
 	public <T extends Component> T getComponent(Class<T> componentType) {
-		return (T) componentStores.get(componentType).get(FIRST_COMPONENT);
+		List<Component> store = componentStores.get(componentType);
+		
+		if(store.size() > 0) {
+			return (T) store.get(FIRST_COMPONENT);
+		}
+		
+		return null;
 	}
 	
 	@SuppressWarnings("unchecked")
 	public <T extends Component> List<T> getAllComponentsOfType(Class<T> componentType) {
 		List<T> store = (List<T>) componentStores.get(componentType);
 
-		if( store == null) {
+		if(store == null) {
 			store = new LinkedList<T>();
 		}
 
