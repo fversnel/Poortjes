4f1cb7f9442fadd8f3b2219f95ea5a4790ab134a
Frank Versnel <f.versnel@gmail.com>
2011-04-02 10:19:25 +0200

    Added a simple proof of concept for using input devices.

---
+++ src/frankversnel/processing/input/InputDevice.java
@@ -0,0 +1,7 @@
+package frankversnel.processing.input;
+
+public interface InputDevice {
+	
+	public void handleInput();
+
+}
---
+++ src/frankversnel/processing/input/Keyboard.java
@@ -0,0 +1,34 @@
+package frankversnel.processing.input;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Keyboard implements KeyListener {
+	private static Logger logger = LoggerFactory.getLogger(Keyboard.class);
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		char keyPressed = arg0.getKeyChar();
+		
+		// DEBUG CODE
+		if(keyPressed == 's') {
+			logger.info("Key " + keyPressed);
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
