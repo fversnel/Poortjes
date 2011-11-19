bc28c342adbde502a32ac3254ff8575508597403
Frank Versnel <f.versnel@gmail.com>
2011-11-19 10:08:01 +0100

    Started fooling around with software transactional memory to have shared state between actors such as speed and transform.

--- src/main/scala/org/frankversnel/poortjes/common/transform.scala
+++ src/main/scala/org/frankversnel/poortjes/common/transform.scala
@@ -1,6 +1,7 @@
 package org.frankversnel.poortjes;
 
 import java.awt.geom.AffineTransform;
+import akka.stm._
 import processing.core.PMatrix;
 import processing.core.PMatrix2D;
 
@@ -9,35 +10,44 @@ import org.frankversnel.poortjes._;
 trait Transform extends Component {
 	val dimension: Dimension
 
-	private val matrix = new PMatrix2D;
+	private val matrixRef = Ref(new PMatrix2D)
 
-	def positionX = matrix.m02
-	def positionY = matrix.m12
-	def scaleX = matrix.m00
-	def scaleY = matrix.m11
+	def positionX = atomic(matrix.m02)
+	def positionY = atomic(matrix.m12)
+	def scaleX = atomic(matrix.m00)
+	def scaleY = atomic(matrix.m11)
 	def width = dimension.width
 	def height = dimension.height
 
-	def translate(x: Float, y: Float) = matrix.translate(x, y)
-	def scale(x: Float, y: Float) = matrix.scale(x, y)
-
-	def rotate(theta: Float) {
-		val matrixCopy = matrix.get
-
-		// To rotate around its own axis we need to put it in local space then rotate and put it
-		// back in global space.
-		matrixCopy.translate(dimension.width / 2, dimension.height / 2)
-		matrixCopy.rotate(theta)
-		matrixCopy.translate(-(dimension.width / 2), -(dimension.height / 2))
-
-		matrix.set(matrixCopy);
+	def translate(x: Float, y: Float) = atomic {
+		alterMatrix(_.translate(x, y))
+	}
+	def scale(x: Float, y: Float) = atomic {
+		alterMatrix(_.scale(x, y))
+	}
+	def rotate(theta: Float) = atomic {
+			alterMatrix { matrix =>
+			// To rotate around its own axis we need to put it in local space then rotate and put it
+			// back in global space.
+			matrix.translate(dimension.width / 2, dimension.height / 2)
+			matrix.rotate(theta)
+			matrix.translate(-(dimension.width / 2), -(dimension.height / 2))
+		}
 	}
 
+	def processingMatrix = matrix
 	def affineTransform(): AffineTransform = {
 		return new AffineTransform(
 				matrix.m00, matrix.m10, matrix.m01,
 				matrix.m11, matrix.m02, matrix.m12)
 	}
 
-	def processingMatrix = matrix
+	private def matrix = matrixRef.get
+	private def alterMatrix(matrixFunction: PMatrix2D => Unit) = atomic {
+		matrixRef alter { matrix =>
+			val newMatrix = matrix.get
+			matrixFunction(newMatrix)
+			newMatrix
+		}
+	}
 }
--- src/main/scala/org/frankversnel/poortjes/moving/speed.scala
+++ src/main/scala/org/frankversnel/poortjes/moving/speed.scala
@@ -1,23 +1,29 @@
 package org.frankversnel.poortjes.moving
 
+import akka.stm._
+
 import org.frankversnel.poortjes._
 
 trait Speed extends Component {
 	val distanceInMs: Float
 	val rotationInMs: Float
 
-	private var currentDistance = 0f
-	private var currentRotation = 0f
+	private val currentDistance = Ref(0f)
+	private val currentRotation = Ref(0f)
 
 	def moveSpeed(amount: Float) {
-		currentDistance = distanceInMs * amount;
+		atomic {
+			currentDistance.set(distanceInMs * amount)
+		}
 	}
 
 	def rotationSpeed(amount: Float) {
-		currentRotation = rotationInMs * amount;
+		atomic {
+			currentRotation.set(rotationInMs * amount)
+		}
 	}
 
-	def getDistance = currentDistance
-	def getRotation = currentRotation
+	def getDistance = atomic(currentDistance.get)
+	def getRotation = atomic(currentRotation.get)
 }
 
