5e1fe0769e47b08ef46e93b0bf61c40db7d1a2b8
Frank Versnel <f.versnel@gmail.com>
2012-05-05 13:57:59 +0200

    Minor tweaks.

--- src/main/scala/org/frankversnel/poortjes/game/game_objects.scala
+++ src/main/scala/org/frankversnel/poortjes/game/game_objects.scala
@@ -83,25 +83,20 @@ trait PlayerKiller extends Collidable {
 class Gate extends Transform with Speed with Moveable {
 	var dimension = (10, 70)
 
-	val distanceInMs = 0.3f
-	val rotationInMs = 0.01f
+	val distanceInMs = 0.1f
+	val rotationInMs = 0.005f
 
 	moveSpeed = (math.random.toFloat, math.random.toFloat)
 	rotationSpeed = 1f
 }
-abstract class GateComponent(private val _parent: Gate)
-		extends DrawableShape with Collidable {
-
-	parent = _parent
-}
-class GateEnd(protected val resourceLoader: ResourceLoader, private val _parent: Gate)
-        extends GateComponent(_parent) with PlayerKiller {
+class GateEnd(protected val resourceLoader: ResourceLoader)
+        extends DrawableShape with PlayerKiller {
 	var dimension = (10, 10)
 
 	protected val shape = resourceLoader.addResource("gate-end.svg")
 }
-class GateConnector(protected val resourceLoader: ResourceLoader, private val _parent: Gate)
-        extends GateComponent(_parent) {
+class GateConnector(protected val resourceLoader: ResourceLoader)
+        extends DrawableShape with Collidable {
 	var dimension = (2, 50)
 
 	protected val shape = resourceLoader.addResource("gate-connector.svg")
@@ -119,11 +114,14 @@ object Gate {
 	def build(resourceLoader: ResourceLoader): Gate = {
 		val gate = new Gate
 
-		val gateEndTop = new GateEnd(resourceLoader, gate)
+		val gateEndTop = new GateEnd(resourceLoader)
+		gateEndTop.parent = gate
 		gateEndTop.rotate((Pi / 2).toFloat)
-		val gateConnector = new GateConnector(resourceLoader, gate)
+		val gateConnector = new GateConnector(resourceLoader)
+		gateConnector.parent = gate
 		gateConnector.translate(4, 10)
-		val gateEndBottom = new GateEnd(resourceLoader, gate)
+		val gateEndBottom = new GateEnd(resourceLoader)
+		gateEndBottom.parent = gate
 		gateEndBottom.translate(0, 60)
 		gateEndBottom.rotate((Pi + (Pi / 2)).toFloat)
 
